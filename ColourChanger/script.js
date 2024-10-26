8ea5de1a73ae3cd114fa2080d9905980041a2a78
nabhajit <git@stackblitz.com>
2024-10-26 15:46:20 +0530

    Initial commit

---
+++ ColourChanger/script.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelectorAll('.colour');
+const body = document.querySelector('body');
+
+buttons.forEach(function(colour) {
+  console.log(colour);
+  colour.addEventListener('click', function(e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'red') {
+      body.style.backgroundColor = e.target.id;
+    }
+    else if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    else if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    else if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    else if (e.target.id === 'purple') {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# Miscellaneous-Javascript-Project
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nabhajit/Miscellaneous-Javascript-Project)
